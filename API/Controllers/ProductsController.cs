4a6bbe971b7eeb7eb75e0549e4b5e47c4b29efd7
erlalit1988 <lalit.k9956@gmail.com>
2025-02-27 11:24:27 +0530

    adding functionality for pagination, sorting and searching

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,22 +2,17 @@
 using Core.Interfaces;
 using Core.Specifications;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics.Eventing.Reader;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController(IGenericRepository<Product> repo) : ControllerBase
+    public class ProductsController(IGenericRepository<Product> repo) : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, 
-            string? sort)
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts([FromQuery] ProductSpecParams specParams)
         {
-            var spec = new ProductSpecification(brand, type, sort);
-
-            var products = await repo.ListAsync(spec);
-            return Ok(products);
+            var spec = new ProductSpecification(specParams);
+             
+            return await CreatePageResult(repo,spec,specParams.PageIndex, specParams.PageSize);
         }
 
         [HttpGet("{id:int}")] // api/products/2
