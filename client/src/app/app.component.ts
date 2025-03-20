c06540c601f38f96b731af0072578bef52bd62f5
erlalit1988 <lalit.k9956@gmail.com>
2025-03-20 09:48:51 +0530

    for Sorting, filtering, pagination, search functionailtiy

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { HeaderComponent } from "./layout/header/header.component";
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'client';
+  title = 'Skinet';
+  
 }
