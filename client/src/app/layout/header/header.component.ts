6ed9dbf8f47e789f5a84c3a375da968be048a2c5
erlalit1988 <lalit.k9956@gmail.com>
2025-03-24 11:54:59 +0530

    added functionality for loading and error handling on front-end

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatIcon} from '@angular/material/icon'
 import {MatButton} from '@angular/material/button'
 import {MatBadge} from '@angular/material/badge'
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { BusyService } from '../../core/services/busy.service';
+import { MatProgressBar } from '@angular/material/progress-bar'
 @Component({
   selector: 'app-header',
   imports: [
@@ -10,11 +12,12 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
     RouterLink,
     MatIcon, 
     MatButton,
-    MatBadge
+    MatBadge,
+    MatProgressBar
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+   busyService = inject(BusyService);
 }
