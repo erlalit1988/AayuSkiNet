3d7076008e1047ae43a6269ef092bf83d0eb1e06
erlalit1988 <lalit.k9956@gmail.com>
2025-04-02 18:53:52 +0530

    adding cors for to handle client cred

--- API/Program.cs
+++ API/Program.cs
@@ -51,8 +51,10 @@ app.MapScalarApiReference(options =>
 }
 app.UseMiddleware<ExceptionMiddleware>();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
-   .WithOrigins("http://localhost:4200","https://localhost:4200"));
+app.UseCors(x => x.AllowAnyHeader()
+.AllowAnyMethod()
+.AllowCredentials()
+.WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
 app.MapGroup("api").MapIdentityApi<AppUser>(); // api/login
